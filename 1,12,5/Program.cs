0461165f2997187bef82d30122243dbba2bdc159
bks1994K <bks1994@mail.ru>
2022-10-05 18:46:16 +0300

    1.12.5

---
+++ 1,12,5/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int left = 1;
+int right = number;
+int i = 0;
+while (i*i*i!=number)
+{
+    i = (right - left + 1) / 2;
+    if (i * i * i > number)
+    {
+        right = i;
+    }
+    else if (i * i * i < number)
+    {
+        left = i;
+    }
+
+}
+    Console.WriteLine(i);
